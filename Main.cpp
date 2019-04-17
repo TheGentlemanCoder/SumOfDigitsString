81b0d61af771b27fc9e91f87bb9d24c7dbc85132
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2019-04-17 11:41:06 -0700

    Add functionality to prompt user for a number

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string numberStr;
+	
+	cout << "Enter a number: ";
+	cin >> numberStr;
+	cout << "You entered: " << numberStr << endl;
+}
